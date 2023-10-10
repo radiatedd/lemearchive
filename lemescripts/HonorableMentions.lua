36a5197f84076580f6d891f856dd15a3d4d708ed
0x59 <103334700+radiatedd@users.noreply.github.com>
2023-10-10 11:42:38 -0700

    Update and rename pub.lua to HonorableMentions.lua

---
+++ lemescripts/HonorableMentions.lua
@@ -0,0 +1,11 @@
+--[[
+This was a script that I accidentally put into lemescripts, so it's going to be a honorable mentions
+
+HONORABLE MENTIONS:
+Mister Charles / for sharing methtools with me
+Leme / The creator of every script in this repository
+
+
+that's it :^)
+
+]]
